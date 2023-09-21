7d7cc0f5178563675c5728e25cf23524a16fe21c
JPercival22 <you@example.com>
2023-09-21 04:39:22 +0100

    removed unused files, created mp3 files, refactored codebase - tuner complete

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
+  console.log("External script loaded");
 
   const navToggle = document.querySelector(".hamburger-menu");
   const links = document.querySelector(".nav-menu");
@@ -124,53 +125,45 @@ if (firstNewTabLink) {
 
 // Advanced Tuner page 
 
-// Advanced Tuner page 
-        // Function to play audio
-        function play(audioSrc) {
-          const audio = new Audio(audioSrc);
-          audio.play();
-      }
-  
+// Get all the guitar selection buttons
+const guitarButtons = document.querySelectorAll('.guitar-select-button');
 
-          // Get all the guitar selection buttons
-          const guitarButtons = document.querySelectorAll('.guitar-select-button');
-  
-          // Get all the interactive placeholders
-          const placeholders = document.querySelectorAll('.interactive-placeholder');
-  
-          // Get all the grid items
-          const gridItems = document.querySelectorAll('.grid-item');
-  
-          // Hide all placeholders except the "Acoustic Guitar" placeholder initially
-          placeholders.forEach((placeholder) => {
-              if (!placeholder.classList.contains('acoustic')) {
-                  placeholder.style.display = 'none';
-              }
-          });
-  
-          // Add the outline to the default "Acoustic Guitar" grid item
-          gridItems[0].classList.add('active-grid-item');
-  
-          // Add a click event listener to each guitar selection button
-          guitarButtons.forEach((button, index) => {
-              button.addEventListener('click', () => {
-                  // Hide all placeholders
-                  placeholders.forEach((placeholder) => {
-                      placeholder.style.display = 'none';
-                  });
-  
-                  // Remove the outline from all grid items
-                  gridItems.forEach((gridItem) => {
-                      gridItem.classList.remove('active-grid-item');
-                  });
-  
-                  // Show the selected placeholder
-                  placeholders[index].style = 'display:flex';
-  
-                  // Add the outline to the selected grid item
-                  gridItems[index].classList.add('active-grid-item');
-              });
-          });
+// Get all the interactive placeholders
+const placeholders = document.querySelectorAll('.interactive-placeholder');
+
+// Get all the grid items
+const gridItems = document.querySelectorAll('.grid-item');
+
+// Hide all placeholders except the "Acoustic Guitar" placeholder initially
+placeholders.forEach((placeholder) => {
+    if (!placeholder.classList.contains('acoustic')) {
+        placeholder.style.display = 'none';
+    }
+});
+
+// Add the outline to the default "Acoustic Guitar" grid item
+gridItems[0].classList.add('active-grid-item');
+
+// Add a click event listener to each guitar selection button
+guitarButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        // Hide all placeholders
+        placeholders.forEach((placeholder) => {
+            placeholder.style.display = 'none';
+        });
+
+        // Remove the outline from all grid items
+        gridItems.forEach((gridItem) => {
+            gridItem.classList.remove('active-grid-item');
+        });
+
+        // Show the selected placeholder
+        placeholders[index].style = 'display:flex';
+
+        // Add the outline to the selected grid item
+        gridItems[index].classList.add('active-grid-item');
+    });
+});
 
 
 });
