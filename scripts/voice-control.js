5447c765bec94bd3dd51f6e4adb3291037e14212
JPercival22 <you@example.com>
2023-10-17 21:38:12 +0100

    initialised annyangang cdn and  basic voice commands inline

--- scripts/voice-control.js
+++ scripts/voice-control.js
@@ -1,93 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('External script loaded');
-  
-    if (typeof annyang !== 'undefined') {
-      var commands = {
-        'open modal': function () {
-          const vcModal = document.getElementById('modal');
-          if (vcModal) {
-            vcModal.style.display = 'flex';
-            alert("Success");
-          }
-        },
-      };
-      annyang.addCommands(commands);
-      annyang.start({ autoRestart: false, continuous: false });
-    } else {
-      console.log('Annyang is not defined. Check if the library is properly loaded.');
-    }
-  });
-  
-            // 'close': function () {
-            //     $("#help-modal").toggleClass('help-center')
-            // },
-            // //commands to open pages
-            // 'go back': function () {
-            //     window.history.back(1)
-            // },
-            // 'go forward': function () {
-            //     window.history.go(1);
-            // },
-            // 'home': function () {
-            //     window.location.assign("/index.html")
-            // },
-            // 'open basics': function () {
-            //     window.location.assign("/basics.html")
-            // },
-            // 'open frets': function () {
-            //     window.location.assign("/frets.html")
-            // },
-            // 'open chords': function () {
-            //     window.location.assign("/chords.html")
-            // },
-            // 'open rhythm': function () {
-            //     window.location.assign("/rhythm.html")
-            // },
-            // 'open lead': function () {
-            //     window.location.assign("/lead.html")
-            // },
-            // 'open second basics lesson': function () {
-            //     window.location.assign("/basics2.html")
-            // },
-            // 'finger positioning': function () {
-            //     window.location.assign("/frets2.html")
-            // },
-            // 'the power chord': function () {
-            //     window.location.assign("/chords2.html")
-            // },
-            // 'first music lesson': function () {
-            //     window.location.assign("/rhythm2.html")
-            // },
-            // 'reading music': function () {
-            //     window.location.assign("/lead2.html")
-            // },
+  console.log('External script loaded');
 
-            // 'go to the top': function () {
-            //     $('html, body').animate({
-            //         scrollTop: 0
-            //     }, 0);
-            // },
-            // 'go to the bottom': function () {
-            //     $('html, body').animate({
-            //         scrollTop: 9999
-            //     }, 0);
-            // },
-            // //opens the mobile menu
-            // 'open menu': function () {
-            //     $("#mobile-menu").toggleClass('mobile-navigation')
-            // },
-            // 'scroll up': function () {
-            //     $('html, body').animate({
-            //         scrollTop: $("html").scrollTop() - 540
-            //     }, 'easeInOutBack');
-            // },
-            // 'scroll down': function () {
-            //     $('html, body').animate({
-            //         scrollTop: $("html").scrollTop() + 540
-            //     }, 'easeInOutBack');
+  // Check if Annyang is available and initialized
+  if (typeof annyang !== 'undefined' && annyang.isListening()) {
+    var commands = {
+      'open modal': function () {
+        const vcModal = document.getElementById('modal');
+        if (vcModal) {
+          vcModal.style.display = 'flex';
+          alert("Success");
+        }
+      },
+    };
 
-            // },
-            // 'E': function () {
-            //     $('tuner-E').click('')
-            // }
+    // Add voice commands
+    annyang.addCommands(commands);
 
+    // Handle error events
+    annyang.addCallback('error', function (error) {
+      console.error('Annyang error:', error);
+    });
+
+    // Start Annyang with continuous listening
+    annyang.start({ autoRestart: true, continuous: true });
+
+    // Log a message when Annyang is listening
+    annyang.addCallback('start', function () {
+      console.log('Annyang is listening...');
+    });
+  } else {
+    console.log('Annyang is not defined or not ready. Check if the library is properly loaded.');
+  }
+});
