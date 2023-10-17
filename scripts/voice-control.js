f93edaa5dafc3b83dc10af48fe613f0ef4bfdd3c
JPercival22 <you@example.com>
2023-10-17 18:28:19 +0100

    applied settings for js configuration due to irrelevant type script checks

--- scripts/voice-control.js
+++ scripts/voice-control.js
@@ -1,61 +1,66 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('External script loaded')
+    console.log('External script loaded');
   
-// Voice command
-
-    if (annyang) {
-
-        var commands = {
-            //Opens the voice command phrase menu
-            'open modal': function () {
-                const vcModal = document.getElementById('modal');
-                vcModal.style.display = 'flex';
-                alert("Success");
-            },
-
-            'close': function () {
-                $("#help-modal").toggleClass('help-center')
-            },
-            //commands to open pages
-            'go back': function () {
-                window.history.back(1)
-            },
-            'go forward': function () {
-                window.history.go(1);
-            },
-            'home': function () {
-                window.location.assign("/index.html")
-            },
-            'open basics': function () {
-                window.location.assign("/basics.html")
-            },
-            'open frets': function () {
-                window.location.assign("/frets.html")
-            },
-            'open chords': function () {
-                window.location.assign("/chords.html")
-            },
-            'open rhythm': function () {
-                window.location.assign("/rhythm.html")
-            },
-            'open lead': function () {
-                window.location.assign("/lead.html")
-            },
-            'open second basics lesson': function () {
-                window.location.assign("/basics2.html")
-            },
-            'finger positioning': function () {
-                window.location.assign("/frets2.html")
-            },
-            'the power chord': function () {
-                window.location.assign("/chords2.html")
-            },
-            'first music lesson': function () {
-                window.location.assign("/rhythm2.html")
-            },
-            'reading music': function () {
-                window.location.assign("/lead2.html")
-            },
+    if (typeof annyang !== 'undefined') {
+      var commands = {
+        'open modal': function () {
+          const vcModal = document.getElementById('modal');
+          if (vcModal) {
+            vcModal.style.display = 'flex';
+            alert("Success");
+          }
+        },
+      };
+      annyang.addCommands(commands);
+      annyang.start({ autoRestart: false, continuous: false });
+    } else {
+      console.log('Annyang is not defined. Check if the library is properly loaded.');
+    }
+  });
+  
+            // 'close': function () {
+            //     $("#help-modal").toggleClass('help-center')
+            // },
+            // //commands to open pages
+            // 'go back': function () {
+            //     window.history.back(1)
+            // },
+            // 'go forward': function () {
+            //     window.history.go(1);
+            // },
+            // 'home': function () {
+            //     window.location.assign("/index.html")
+            // },
+            // 'open basics': function () {
+            //     window.location.assign("/basics.html")
+            // },
+            // 'open frets': function () {
+            //     window.location.assign("/frets.html")
+            // },
+            // 'open chords': function () {
+            //     window.location.assign("/chords.html")
+            // },
+            // 'open rhythm': function () {
+            //     window.location.assign("/rhythm.html")
+            // },
+            // 'open lead': function () {
+            //     window.location.assign("/lead.html")
+            // },
+            // 'open second basics lesson': function () {
+            //     window.location.assign("/basics2.html")
+            // },
+            // 'finger positioning': function () {
+            //     window.location.assign("/frets2.html")
+            // },
+            // 'the power chord': function () {
+            //     window.location.assign("/chords2.html")
+            // },
+            // 'first music lesson': function () {
+            //     window.location.assign("/rhythm2.html")
+            // },
+            // 'reading music': function () {
+            //     window.location.assign("/lead2.html")
+            // },
 
             // 'go to the top': function () {
             //     $('html, body').animate({
@@ -85,15 +90,4 @@ document.addEventListener('DOMContentLoaded', () => {
             // 'E': function () {
             //     $('tuner-E').click('')
             // }
-        };
-        annyang.addCommands(commands);
-        annyang.start({ autoRestart: false, continuous: false });
-        //       // Tell KITT to use annyang
-        // SpeechKITT.annyang();
-        //   // Define a stylesheet for KITT to use
-        // SpeechKITT.setStylesheet('//cdnjs.cloudflare.com/ajax/libs/SpeechKITT/1.0.0/themes/flat.css');
-        //   // Render KITT's interface
-        // SpeechKITT.vroom();
-    }
-    // $("#skitt-listening-text__instructions").text("say \"open help center\"");
-})
+
